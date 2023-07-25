1d910913e92a515519f5b6aa7547cc593f38478c
Jaismeen <jaismeenmehra9@gmail.com>
2023-07-25 07:47:16 +0530

    added props and proptypes to Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+export default function Navbar(props) {
+  return (
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a className="navbar-brand" href="/">
+          {props.brand}
+        </a>
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Features
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              {props.about}
+            </a>
+          </li>
+          
+        </ul>
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  )
+}
+
+
+Navbar.proptypes ={brand: PropTypes.string,
+                    about: PropTypes.string}
